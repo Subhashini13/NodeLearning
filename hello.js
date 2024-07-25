5488ddb67e7d2fb704b2bb915c5e35a2a2a9e2f4
Subhashini Kanth <kanthsubhashini@gmail.com>
2024-07-25 18:33:03 +0530

    first commit 25july

---
+++ hello.js
@@ -0,0 +1,8 @@
+//console.log("Hey There ! I am Learning Nodejs on 25th July 2024");
+
+
+// console.log(window);  --> not run here , will run in browser only
+
+// console.log(alert('Hey')); --> not run here,
+const math = require('./math');
+console.log("Math value is ", math(2,4));
